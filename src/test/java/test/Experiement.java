73368384a6276ad17455490c6c4413d2f128f1c6
Pierre <pi.lequay@gmail.com>
2021-07-27 12:22:40 +0200

    Pierre

--- src/test/java/test/Experiement.java
+++ src/test/java/test/Experiement.java
@@ -4,6 +4,11 @@ public class Experiement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int attaque=101;
+		int nbBatiment=10;
+		int i = attaque%nbBatiment;
+		int r = attaque- i;
+		System.out.println(r);
 
 	}
 
